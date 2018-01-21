a15a265337ba9107fe532d75e99bd4a758c3d281
Damien Bargiacchi <icymidnight@gmail.com>
2018-01-21 05:49:01 -0800

    Add some more comments

--- selfdesc.py
+++ selfdesc.py
@@ -3,12 +3,13 @@
 import sys
 import random
 
-# This will explode us if we happen to get all 10 digits to be the same value. Hopefully that doesn't happen! :P
+# This will explode us if we happen to get all 10 digits to be the same value. Good luck! :P
 def randomize_counts(counts):
   for i in xrange(10):
     counts[i] = random.randint(0,9)
 
 # Find a self describing 10 digit number such that pos 0 is a count of 0s, pos 1 is a count of 1s, etc
+# Because of https://www.youtube.com/watch?v=1GKfEDvhWdY
 def main():
   hist = []
   counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -21,8 +22,11 @@ def main():
       print "We did it!"
       print counts
       sys.exit(0);
+
     counts = new_counts
     print counts
+
+    # Detect loops. If we're stuck in a loop jump to a random number and start over.
     for a in hist:
       if counts == a:
         print "loop!"
